315c8ece4a6df9203050878e12a808f8cb042234
BT_1N3_17 <jliu2882@bths.edu>
2018-11-14 11:39:43 -0500

    selection sort

--- src/jliu2882/Main.java
+++ src/jliu2882/Main.java
@@ -25,4 +25,12 @@ public class Main {
             }
         }
     }
+
+    public static void selectionSort(int[] arr){
+        for(int i = 0; i < arr.length; i ++){
+            for(int j = 0; j < arr.length; j++){
+                
+            }
+        }
+    }
 }
