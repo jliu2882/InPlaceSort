059a2952a7fd3af0830a3f9ba73a2971c6c778b3
BT_1N3_15 <jliu2882@bths.edu>
2018-11-16 11:38:04 -0500

    renamed arr parameter to list1 for rubric(?)

--- src/jliu2882/InPlaceSorts.java
+++ src/jliu2882/InPlaceSorts.java
@@ -1,44 +1,44 @@
 package jliu2882;
 
 public class InPlaceSorts {
-    public static void swap(int arr[], int i, int j){
-        int temp = arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
+    public static void swap(int list1[], int i, int j){
+        int temp = list1[i];
+        list1[i]=list1[j];
+        list1[j]=temp;
     }
-    public static void swap(double arr[], int i, int j){
-        double temp = arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
+    public static void swap(double list1[], int i, int j){
+        double temp = list1[i];
+        list1[i]=list1[j];
+        list1[j]=temp;
     }
-    public static void swap(String arr[], int i, int j){
-        String temp = arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
+    public static void swap(String list1[], int i, int j){
+        String temp = list1[i];
+        list1[i]=list1[j];
+        list1[j]=temp;
     }
 
     public static int randInt(int max){
         return (int)(Math.random()*(max+1));
     }
     public static int[] randIntArr(int count, int max){
-        int[] arr;
-        arr = new int[count];
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = randInt(max);
+        int[] list1;
+        list1 = new int[count];
+        for(int i = 0; i < list1.length; i++){
+            list1[i] = randInt(max);
         }
-        return arr;
+        return list1;
     }
 
     public static double randDoub(int max){
         return Math.random()*(max+1);
     }
     public static double[] randDoubArr(int count, int max){
-        double[] arr;
-        arr = new double[count];
-        for(int i = 0; i < arr.length;i++){
-            arr[i] = randDoub(max);
+        double[] list1;
+        list1 = new double[count];
+        for(int i = 0; i < list1.length;i++){
+            list1[i] = randDoub(max);
         }
-        return arr;
+        return list1;
     }
 
     public static String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
@@ -51,41 +51,41 @@ public class InPlaceSorts {
         return result;
     }
     public static String[] randStrArr(int count){
-        String[] arr;
-        arr = new String[count];
-        for(int i =0; i < arr.length;i++){
-            arr[i] = randStr();
+        String[] list1;
+        list1 = new String[count];
+        for(int i =0; i < list1.length;i++){
+            list1[i] = randStr();
         }
-        return arr;
+        return list1;
     }
 
 
-    public static void bubbleSort(String[] arr){
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr.length-1; j++) {
-                if (arr[j + 1].compareTo(arr[j])<0) {
-                    swap(arr, j, j + 1);
+    public static void bubbleSort(String[] list1){
+        for(int i = 0; i < list1.length; i++){
+            for(int j = 0; j < list1.length-1; j++) {
+                if (list1[j + 1].compareTo(list1[j])<0) {
+                    swap(list1, j, j + 1);
                 }
             }
         }
     }
-    public static void selectionSort(double[] arr){
-        for(int i = 0; i < arr.length; i ++){
-            for(int j = i + 1; j < arr.length; j++){
-                if(arr[i]>arr[j]){
-                    swap(arr,i,j);
+    public static void selectionSort(double[] list1){
+        for(int i = 0; i < list1.length; i ++){
+            for(int j = i + 1; j < list1.length; j++){
+                if(list1[i]>list1[j]){
+                    swap(list1,i,j);
                 }
             }
         }
     }
-    public static void insertionSort(int[] arr){
-        for(int i = 1; i < arr.length; i ++){
-            int temp = arr[i];
+    public static void insertionSort(int[] list1){
+        for(int i = 1; i < list1.length; i ++){
+            int temp = list1[i];
             int j;
-            for(j = i-1; j>=0 && temp  < arr[j]; j--){
-                arr[j+1] = arr[j];
+            for(j = i-1; j>=0 && temp  < list1[j]; j--){
+                list1[j+1] = list1[j];
             }
-            arr[j+1] = temp;
+            list1[j+1] = temp;
         }
     }
 }
